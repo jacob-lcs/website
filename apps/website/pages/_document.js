4ffcf519e5c7ccf1e59ee37780c73b46dd8893eb
Jacob <jacob.lcs@qq.com>
2025-01-19 09:05:30 +0800

    feat: Add Google Ads script to document head

--- apps/website/pages/_document.js
+++ apps/website/pages/_document.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { SkipNavLink } from '@reach/skip-nav'
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { SkipNavLink } from "@reach/skip-nav";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7669803939408201"
+            crossorigin="anonymous"
+          ></script>
+        </Head>
         <body>
           <SkipNavLink />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
